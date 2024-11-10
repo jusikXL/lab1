3341dcf09d9ffcc6f39a8b02e03fba5c71c7fcf6
jusikXL <125464973+jusikXL@users.noreply.github.com>
2024-11-10 12:45:27 +0100

    init

---
+++ src/SetTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+public class SetTest {
+  @Test
+  void testDistinctClosed() {
+
+  }
+
+  @Test
+  void testInsert() {
+
+  }
+
+  @Test
+  void testIntersect() {
+
+  }
+
+  @Test
+  void testMember() {
+
+  }
+
+  @Test
+  void testToArray() {
+
+  }
+}
