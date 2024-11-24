2c1cff216d05c0fa3faeca1b3fa00defb2329ec2
jusikXL <125464973+jusikXL@users.noreply.github.com>
2024-11-24 12:43:32 +0100

    commit

--- src/SetRequiredNumber.java
+++ src/SetRequiredNumber.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 
-
 class SetRequiredNumber {
     static final int SIZE = 8; // Number of hours in the schedule
     static final int REQUIRED_NUMBER = 10; // Employees required per hour
@@ -13,8 +12,8 @@ class SetRequiredNumber {
 
     @BeforeEach
     void setUp() {
-        schedule = new WorkSchedule(SIZE);  
-        
+        schedule = new WorkSchedule(SIZE);
+
         schedule.setRequiredNumber(REQUIRED_NUMBER, 0, SIZE - 1);
         for (int i = 0; i < REQUIRED_NUMBER; i++) {
             schedule.addWorkingPeriod(String.valueOf(i), 0, SIZE - 1);
@@ -26,16 +25,17 @@ class SetRequiredNumber {
     void test(int startTime, int endTime, int nemployee) {
         try {
             schedule.setRequiredNumber(nemployee, startTime, endTime); // should do nothing
-        } catch (Exception e) {}
+        } catch (Exception e) {
+        }
 
         for (int i = 0; i < SIZE; i++) {
             WorkSchedule.Hour hour = schedule.readSchedule(i);
 
-            if(startTime > endTime || startTime >= SIZE || endTime >= SIZE) {
+            if (startTime > endTime || startTime >= SIZE || endTime >= SIZE) {
                 assertEquals(REQUIRED_NUMBER, hour.requiredNumber);
                 assertEquals(REQUIRED_NUMBER, hour.workingEmployees.length);
                 // unchanged
-            } else if(i >= startTime && i <= endTime) {
+            } else if (i >= startTime && i <= endTime) {
                 // changed
                 assertEquals(nemployee, hour.requiredNumber);
                 assertEquals(nemployee > REQUIRED_NUMBER ? REQUIRED_NUMBER : nemployee, hour.workingEmployees.length);
@@ -50,43 +50,43 @@ class SetRequiredNumber {
     // TODO: refactor with https://junit.org/junit5/docs/current/user-guide/
     static Stream<Arguments> provider() {
         return Stream.of(
-            // startTime, endTime, nemployee
-            Arguments.of(SIZE - 1, SIZE / 2, REQUIRED_NUMBER), // start > end
-            Arguments.of(SIZE, SIZE / 2, REQUIRED_NUMBER), // start >= size
-            Arguments.of(SIZE / 2, SIZE, REQUIRED_NUMBER), // end >= size
+                // startTime, endTime, nemployee
+                Arguments.of(SIZE - 1, SIZE / 2, REQUIRED_NUMBER), // start > end
+                Arguments.of(SIZE, SIZE + 1, REQUIRED_NUMBER), // start >= size
+                Arguments.of(SIZE / 2, SIZE, REQUIRED_NUMBER), // end >= size
 
-            Arguments.of(1, SIZE - 2, REQUIRED_NUMBER), // inside
-            Arguments.of(0, SIZE / 2, REQUIRED_NUMBER), // start to middle
-            Arguments.of(SIZE / 2, SIZE - 1, REQUIRED_NUMBER), // middle to end
-            Arguments.of(0, SIZE - 1, REQUIRED_NUMBER), // start to end
+                Arguments.of(1, SIZE - 2, REQUIRED_NUMBER), // inside
+                Arguments.of(0, SIZE / 2, REQUIRED_NUMBER), // start to middle
+                Arguments.of(SIZE / 2, SIZE - 1, REQUIRED_NUMBER), // middle to end
+                Arguments.of(0, SIZE - 1, REQUIRED_NUMBER), // start to end
 
-            Arguments.of(SIZE / 2, SIZE / 2, REQUIRED_NUMBER), // start = end
+                Arguments.of(SIZE / 2, SIZE / 2, REQUIRED_NUMBER), // start = end
 
-            //
+                //
 
-            Arguments.of(SIZE - 1, SIZE / 2, REQUIRED_NUMBER / 2), // start > end
-            Arguments.of(SIZE, SIZE / 2, REQUIRED_NUMBER / 2), // start >= size
-            Arguments.of(SIZE / 2, SIZE, REQUIRED_NUMBER / 2), // end >= size
+                Arguments.of(SIZE - 1, SIZE / 2, REQUIRED_NUMBER / 2), // start > end
+                Arguments.of(SIZE, SIZE + 1, REQUIRED_NUMBER / 2), // start >= size
+                Arguments.of(SIZE / 2, SIZE, REQUIRED_NUMBER / 2), // end >= size
 
-            Arguments.of(1, SIZE - 2, REQUIRED_NUMBER / 2), // inside
-            Arguments.of(0, SIZE / 2, REQUIRED_NUMBER / 2), // start to middle
-            Arguments.of(SIZE / 2, SIZE - 1, REQUIRED_NUMBER / 2), // middle to end
-            Arguments.of(0, SIZE - 1, REQUIRED_NUMBER / 2), // start to end
+                Arguments.of(1, SIZE - 2, REQUIRED_NUMBER / 2), // inside
+                Arguments.of(0, SIZE / 2, REQUIRED_NUMBER / 2), // start to middle
+                Arguments.of(SIZE / 2, SIZE - 1, REQUIRED_NUMBER / 2), // middle to end
+                Arguments.of(0, SIZE - 1, REQUIRED_NUMBER / 2), // start to end
 
-            Arguments.of(SIZE / 2, SIZE / 2, REQUIRED_NUMBER / 2), // start = end
+                Arguments.of(SIZE / 2, SIZE / 2, REQUIRED_NUMBER / 2), // start = end
 
-            //
+                //
 
-            Arguments.of(SIZE - 1, SIZE / 2, REQUIRED_NUMBER * 2), // start > end
-            Arguments.of(SIZE, SIZE / 2, REQUIRED_NUMBER * 2), // start >= size
-            Arguments.of(SIZE / 2, SIZE, REQUIRED_NUMBER * 2), // end >= size
+                Arguments.of(SIZE - 1, SIZE / 2, REQUIRED_NUMBER * 2), // start > end
+                Arguments.of(SIZE, SIZE + 1, REQUIRED_NUMBER * 2), // start >= size
+                Arguments.of(SIZE / 2, SIZE, REQUIRED_NUMBER * 2), // end >= size
 
-            Arguments.of(1, SIZE - 2, REQUIRED_NUMBER * 2), // inside
-            Arguments.of(0, SIZE / 2, REQUIRED_NUMBER * 2), // start to middle
-            Arguments.of(SIZE / 2, SIZE - 1, REQUIRED_NUMBER * 2), // middle to end
-            Arguments.of(0, SIZE - 1, REQUIRED_NUMBER * 2), // start to end
+                Arguments.of(1, SIZE - 2, REQUIRED_NUMBER * 2), // inside
+                Arguments.of(0, SIZE / 2, REQUIRED_NUMBER * 2), // start to middle
+                Arguments.of(SIZE / 2, SIZE - 1, REQUIRED_NUMBER * 2), // middle to end
+                Arguments.of(0, SIZE - 1, REQUIRED_NUMBER * 2), // start to end
 
-            Arguments.of(SIZE / 2, SIZE / 2, REQUIRED_NUMBER * 2) // start = end
+                Arguments.of(SIZE / 2, SIZE / 2, REQUIRED_NUMBER * 2) // start = end
         );
     }
 }
