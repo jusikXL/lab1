dad1fd71aebc6bdb0c563697601956735091a443
jusikXL <125464973+jusikXL@users.noreply.github.com>
2024-11-25 14:56:23 +0100

    fix

--- src/DistinctClosedTest.java
+++ src/DistinctClosedTest.java
@@ -1,12 +1,8 @@
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import java.util.function.IntBinaryOperator;
-import java.util.stream.Stream;
-
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
 
 public class DistinctClosedTest {
   Set set;
@@ -22,15 +18,8 @@ public class DistinctClosedTest {
     substract = (a, b) -> a - b;
   }
 
-  static Stream<IntBinaryOperator> operationsProvider() {
-    return Stream.of(
-        (a, b) -> a + b,
-        (a, b) -> a - b);
-  }
-
-  @ParameterizedTest
-  @MethodSource("operationsProvider")
-  public void testDistinctClosed(IntBinaryOperator operation) {
+  @Test
+  public void testDistinctClosedAdd() {
     // don't go into for loops
     assertTrue(set.distinctClosed(add)); // statement 1, 5
 
@@ -38,11 +27,30 @@ public class DistinctClosedTest {
     set.insert(0);
     assertTrue(set.distinctClosed(add)); // statement 1, 5
 
-    // go into and out of the i and j for loops
+    // go into and out of only the i and j for loop
     set.insert(1);
     assertTrue(set.distinctClosed(add)); // statement 1, 2, 3, 5
 
-    // go into the if
+    // go into i and j for loops and if
+    set.insert(2);
+    assertFalse(set.distinctClosed(add)); // statement 1, 2, 3, 4
+  }
+
+  @Test
+  public void testDistinctClosedSubstract() {
+    // don't go into for loops
+    assertTrue(set.distinctClosed(substract)); // statement 1, 5
+
+    // go into and out of only the i for loop
+    set.insert(0);
+    assertTrue(set.distinctClosed(substract)); // statement 1, 5
+
+    // go into and out of only the i and j for loop
+    set.insert(1);
+    set.insert(-1);
+    assertTrue(set.distinctClosed(add)); // statement 1, 2, 3, 5
+
+    // go into i and j for loops and if
     set.insert(2);
     assertFalse(set.distinctClosed(add)); // statement 1, 2, 3, 4
   }
